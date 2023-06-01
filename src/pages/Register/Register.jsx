d8cdfa479bdef6a39ad16ef327a44bf4d828e9a6
unknown <esteban.olaya@dominioestudio.com>
2023-06-01 00:25:56 -0500

    Se ajusta formulario de registro

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,11 @@
-
-// Dependencies
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import './Register.scss';
 import axios from 'axios';
 
 const Register = () => {
+    const [emailError, setEmailError] = useState(false);
+
     const onSubmit = (e) => {
         e.preventDefault();
         const cedula = e.target.cedula.value;
@@ -17,6 +17,13 @@ const Register = () => {
         const correo = e.target.email.value;
         const contrasena = e.target.password.value;
         const contrasena2 = e.target.password2.value;
+
+        // Validación de correo electrónico
+        const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!emailRegex.test(correo)) {
+            setEmailError(true);
+            return;
+        }
 
         const user = {
             cedula,
@@ -30,67 +37,58 @@ const Register = () => {
             contrasena2
         }
 
-        axios.post('https://distriplay-back.azurewebsites.net/clientes', user).then((response) => {
-            console.log(response);
-            window.location.href = '/login';
-        }
-        ).catch((err) => {
-            console.log(err);
-        }
-        );
+        axios.post('https://distriplay-back.azurewebsites.net/clientes', user)
+            .then((response) => {
+                console.log(response);
+                window.location.href = '/login';
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 
     return (
         <div>
-            <Header/>
-            <div className="register-container">
-                <div className="register-form-container">
-                    <h2>Registro</h2>
-                    <form className="register-form" onSubmit={(e) => onSubmit(e)}>
-                        <div className="register-form-item">
-                            <label htmlFor="cedula">Cedula</label>
-                            <input type="text" name="cedula" id="cedula" required/>
-                        </div>
-                        <div className="register-form-item">
-                            <label htmlFor="nombre">Nombres</label>
-                            <input type="text" name="nombre" id="nombre" required/>
-                        </div>
-                        <div className="register-form-item">
-                            <label htmlFor="apellido">Apellidos</label>
-                            <input type="text" name="apellido" id="apellido" required/>
-                        </div>
-                        <div className="register-form-item">
-                            <label htmlFor="usuario">Usuario</label>
-                            <input type="text" name="usuario" id="usuario" required/>
-                        </div>
-                        <div className="register-form-item">
-                            <label htmlFor="telefono">Teléfono</label>
-                            <input type="text" name="telefono" id="telefono" required/>
-                        </div>
-                        <div className="register-form-item">
-                            <label htmlFor="pais">Pais</label>
-                            <input type="text" name="pais" id="pais" required/>
-                        </div>
-                        <div className="register-form-item">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email" required/>
-                        </div>
-                        <div className="register-form-item">
-                            <label htmlFor="password">Contraseña</label>
-                            <input type="password" name="password" id="password" required/>
-                        </div>
-                        <div className="register-form-item">
-                            <label htmlFor="password2">Confirmar contraseña</label>
-                            <input type="password" name="password2" id="password2" required/>
-                        </div>
-                        <div className="register-form-item">
-                            <button type="submit">Registrarse</button>
-                        </div>
-                    </form>
-                </div>
+        <Header />
+        <div className="register-container">
+            <div className="register-form-container">
+                <h2>Registro</h2>
+                <form className="register-form" onSubmit={onSubmit}>
+                    <div className="register-form-item">
+                        <input type="text" name="cedula" id="cedula" required placeholder="Cédula" />
+                    </div>
+                    <div className="register-form-item">
+                        <input type="text" name="nombre" id="nombre" required placeholder="Nombres" />
+                    </div>
+                    <div className="register-form-item">
+                        <input type="text" name="apellido" id="apellido" required placeholder="Apellidos" />
+                    </div>
+                    <div className="register-form-item">
+                        <input type="text" name="usuario" id="usuario" required placeholder="Usuario" />
+                    </div>
+                    <div className="register-form-item">
+                        <input type="text" name="telefono" id="telefono" required placeholder="Teléfono" />
+                    </div>
+                    <div className="register-form-item">
+                        <input type="text" name="pais" id="pais" required placeholder="País" />
+                    </div>
+                    <div className="register-form-item">
+                        <input type="email" name="email" id="email" required placeholder="Email" />
+                        {emailError && <span className="error">Por favor, ingresa un correo electrónico válido.</span>}
+                    </div>
+                    <div className="register-form-item">
+                        <input type="password" name="password" id="password" required placeholder="Contraseña" />
+                    </div>
+                    <div className="register-form-item">
+                        <input type="password" name="password2" id="password2" required placeholder="Confirmar contraseña" />
+                    </div>
+                    <div className="register-form-item">
+                        <button type="submit">Registrarse</button>
+                    </div>
+                </form>
             </div>
         </div>
+    </div>
     )
 }
-
 export default Register;
